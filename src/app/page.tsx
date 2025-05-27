34259a87581977f85c56396774791d4a289849c8
Ela Yildiz <eyil061@aucklanduni.ac.nz>
2025-05-28 10:31:44 +1200

    Added basic link to sign up page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <h1>Rainbow Engineering!</h1>
+    <>
+      <h1>Rainbow Engineering!</h1>
+      <Link href="/signup">
+        <button>Sign Up</button>
+      </Link>
+    </>
   );
 }
