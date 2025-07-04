828ff7953599e3203460aafe49d683c0458112a1
vi0letchen <violethongjinchen@gmail.com>
2025-07-05 00:40:30 +1200

    Updated the tail to svg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
           />
       </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#f1eafb]`}
       >
         {children}
         <div className="min-h-screen flex flex-col">
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,26 +4,45 @@ export default function Footer() {
   return (
     <footer className="relative w-full overflow-hidden">
 
+<div className="absolute top-[30%] w-35 h-100 bg-[#d0b7f4] z-0"></div>
+     <div className="absolute top-[35%] w-36 h-100 bg-[#d0b7f4] z-0"></div>
+     <div className="absolute top-[39%] w-37 h-100 bg-[#d0b7f4] z-0"></div>
+     <div className="absolute top-[46%] w-38 h-100 bg-[#d0b7f4] z-0"></div>
+     <div className="absolute top-[53%] w-40 h-100 bg-[#d0b7f4] z-0"></div>
+    <div className="absolute top-[56%] w-41 h-100 bg-[#d0b7f4] z-0"></div>
+    <div className="absolute top-[58%] w-43 h-100 bg-[#d0b7f4] z-0"></div>
+    <div className="absolute top-[63%] w-47 h-100 bg-[#d0b7f4] z-0"></div>
+    <div className="absolute top-[65%] w-54 h-100 bg-[#d0b7f4] z-0"></div>
+
+
      <div className="relative z-30 flex">
   <img
     src="/rainbow_head.png"
     alt="Rainbow Footer Banner"
-    className="relative w-56 object-contain block shrink-0 -mb-[6px]"
+    className="relative w-56 object-contain -mb-[6px]"
   />
-    <div className="flex flex-col justify-start shrink-0 mt-22 -ml-[6px]">
-    <div className="h-[10.5px] mb-[1.8px] bg-[#f35354] w-[5000px]"></div> 
-    <div className="h-[10.1px] mb-[1.8px] bg-[#fcb353] w-[5000px]"></div> 
-    <div className="h-[10px] mb-[1.6px] bg-[#fcf474] w-[5000px]"></div>
-    <div className="h-[10.2px] mb-[1.8px] bg-[#6bbc8c] w-[5000px]"></div>
-    <div className="h-[10px] mb-[1.8px] bg-[#638cec] w-[5000px]"></div> 
-    <div className="h-[9.8px] bg-[#ac4cc4] w-[5000px]"></div>
+
+  <div className="mt-19.5 -ml-[6px] w-full">
+    <svg
+      viewBox="0 0 500 80"
+      xmlns="http://www.w3.org/2000/svg"
+      className="w-full h-[80px]"
+      preserveAspectRatio="none"
+    >
+      <rect y="10.2" width="500" height="10.5" fill="#f35354" />
+      <rect y="22.8" width="500" height="10.1" fill="#fcb353" />
+      <rect y="34.6" width="500" height="10" fill="#fcf474" />
+      <rect y="46.2" width="500" height="10.2" fill="#6bbc8c" />
+      <rect y="58.2" width="500" height="10" fill="#638cec" />
+      <rect y="70.2" width="500" height="9.8" fill="#ac4cc4" />
+    </svg>
   </div>
 
 </div>
 
 
 <div className="bg-[#d0b7f4] text-[#8955d2] text-center p-8 pt-1 relative z-0">
-  <div className="w-full max-w-3xl mx-auto text-left mb-9 my-2">
+  <div className="w-full max-w-3xl mx-auto text-left mb-4 my-2">
   </div>
   <div className="grid font-['Montserrat'] font-bold text-sm grid-cols-2 justify-items-center gap-y-1 gap-x-10 w-full max-w-3xl mx-auto">
 
