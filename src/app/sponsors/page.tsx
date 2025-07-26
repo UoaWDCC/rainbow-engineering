a1dae232f4a6d56dad7c0a3229c07dfad84d66e4
Jordan Yee <jordanyee2003@gmail.com>
2025-07-27 03:47:30 +1200

    Added page and sponsor tier titles

--- src/app/sponsors/page.tsx
+++ src/app/sponsors/page.tsx
@@ -5,6 +5,12 @@ export default function Sponsors() {
     return (
         <div>
             <Banner />
+            <div className="max-w-7xl mx-auto px-4 text-purple-700">
+                <h2 className="text-5xl font-bold mb-3 text-center p-6">OUR SPONSORS</h2>
+                <h2 className="text-5xl font-bold text-[#c2ac63]">GOLD</h2>
+                <h2 className="text-5xl font-bold text-[#babac3]">SILVER</h2>
+                <h2 className="text-5xl font-bold text-[#ae2bfa]">AMETHYST</h2>
+            </div>
         </div>
     );
 }
