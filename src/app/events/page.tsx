2211a70110b66b5e84fc87265f5bec23f5f6a49d
Floppa <cben560@aucklanduni.ac.nz>
2025-08-18 14:07:58 +1200

    Refactor event page

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -8,9 +8,9 @@ export default function Events() {
         <>
             <Banner />
             <div className="bg-purple-100 py-10 px-4 max-w-7xl mx-auto">
-            <h1 className="text-3xl font-bold text-left text-purple-800 font-[Montserrat] mb-8">UPCOMING EVENTS</h1>
+            <h1 className="text-5xl font-bold text-purple-800 font-[Montserrat] mb-8 text-center">UPCOMING EVENTS</h1>
             
-            <EventDropdown 
+            <EventDropdown
                 title="Safe Space" 
                 date="01/08/25" 
                 description="A welcoming and supportive environment for discussion and community building." 
