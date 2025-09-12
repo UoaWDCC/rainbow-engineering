3de00c8041adc6f4d4be5920984da5b42b88fd6b
KokoOCE <79084769+KokoOCE@users.noreply.github.com>
2025-09-12 22:47:24 +1200

    Fixed coloring of header and scrolling gallery
    Changed headers/scrolling gallery button color codes to the darker purple

--- src/app/components/Banner.tsx
+++ src/app/components/Banner.tsx
@@ -8,7 +8,7 @@ export default function Banner({ title }: BannerProps) {
   return (
     <>
       <div id="banner" className="mt-20 bg-[#F1EAFB] w-full px-4 py-10 relative -z-10">
-        <h1 className="text-6xl text-[#8955d2] font-bold font-[Montserrat] py-10 z-10">
+        <h1 className="text-6xl text-[#5f249f] font-bold font-[Montserrat] py-10 z-10">
           {title}
         </h1>
       </div>
