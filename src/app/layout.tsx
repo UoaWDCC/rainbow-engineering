7ed7f70868a39dfe89c7c97c844da2d8e00332c1
Floppa <cben560@aucklanduni.ac.nz>
2025-09-15 17:33:15 +1200

    install payload and refactor file structure
    hope this works

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,10 @@
 import "./globals.css";
-import NavBar from './components/Navbar';
-import Footer from './components/Footer';
-// import Geist fonts via CDN in <head> instead of using 'geist/font'
-import { Montserrat } from 'next/font/google'
+import type { ReactNode } from "react";
 
-const montserrat = Montserrat({
-  subsets: ['latin'],
-  weight: ['100', '400', '700', '900'],
-  variable: '--font-montserrat',
-})
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body
-        className={`${montserrat.variable} antialiased bg-[#f1eafb]`}>
-        <header>
-          <NavBar />
-        </header>
-        {children}
-        <div className="flex flex-col">
-          <main className="flex-grow">
-          </main>
-          <Footer />
-        </div>
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
