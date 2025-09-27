1bf848ef12d3d1d18ad6865177bca25398f865cb
robinthepyro <robinblue072@gmail.com>
2025-09-27 18:11:32 +1200

    Fixed broken payload config

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -11,10 +11,10 @@ import { Users } from "./collections/Users";
 import { Media } from "./collections/Media";
 import { Executives } from "./collections/Executives";
 import { Showcase } from "./collections/Showcase";
-import { AboutUs } from './globals/AboutUs'
-import { Users } from './collections/Users'
-import { Media } from './collections/Media'
-import { Executives } from './collections/Executives'
+import { AboutUs } from "./globals/AboutUs";
+import { Users } from "./collections/Users";
+import { Media } from "./collections/Media";
+import { Executives } from "./collections/Executives";
 
 const filename = fileURLToPath(import.meta.url);
 const dirname = path.dirname(filename);
@@ -26,17 +26,8 @@ export default buildConfig({
       baseDir: path.resolve(dirname),
     },
   },
-<<<<<<< HEAD
+  globals: [AboutUs],
   collections: [Users, Media, Executives, Showcase],
-=======
-  globals: [
-    AboutUs,
-  ],
-  collections: [Users, 
-                Media,
-                Executives,
-               ],
->>>>>>> ff294d48e1df88acd2a4481e0606ca24cc179909
   editor: lexicalEditor(),
   secret: process.env.PAYLOAD_SECRET || "",
   typescript: {
