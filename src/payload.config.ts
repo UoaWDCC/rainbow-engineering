00dc9ed01098cc6b24231e3ef1c9b3ed99f6dc78
KokoOCE <79084769+KokoOCE@users.noreply.github.com>
2025-09-29 03:09:00 +1300

    revert changes to collection name

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -13,7 +13,7 @@ import { Users } from "./collections/Users";
 import { Media } from "./collections/Media";
 import { Executives } from "./collections/Executives";
 import { Showcase } from "./collections/Showcase";
-import { PayloadInitiatives } from "./collections/Initiatives";
+import { Initiatives } from "./collections/Initiatives";
 import { SponsorsAmethyst } from "./collections/SponsorsAmethyst";
 import { SponsorsSilver } from "./collections/SponsorsSilver";
 
