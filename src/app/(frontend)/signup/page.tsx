f1251f7f91831bd5e5e44e7c6637017aabbb0ba1
VriskaSekret <148964668+VriskaSekret@users.noreply.github.com>
2025-10-17 16:54:49 +1300

    Signup page scrollbars removed on mobile

--- src/app/(frontend)/signup/page.tsx
+++ src/app/(frontend)/signup/page.tsx
@@ -2,12 +2,12 @@ import Banner from "../components/Banner"
 import Image from 'next/image'
 
 export default function SignUp() {
-    return (
-        <div>
+  return (
+    <div className="overflow-x-hidden">
           <Banner title="SIGN UP"/>
-    <div className="font-['Montserrat'] text-[30px] text-[#8955d2] font-bold flex justify-center mb-5">FOLLOW OUR SOCIALS</div>
+  <div className="font-['Montserrat'] text-[30px] text-[#8955d2] font-bold flex justify-center mb-5">FOLLOW OUR SOCIALS</div>
     
-<div className="grid grid-cols-2 gap-4 justify-items-center mx-auto max-w-50 lg:max-w-100  lg:mt-10 mb-20">
+<div className="grid grid-cols-2 gap-4 justify-items-center mx-auto max-w-50 lg:max-w-100 lg:mt-10 mb-20 px-2 sm:px-0">
   <div className="flex flex-col items-center">
     <a
       href="https://www.instagram.com/rainbowengineering.uoa?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="
@@ -83,18 +83,16 @@ export default function SignUp() {
   
 </div>
 
-          <div className="bg-fixed md:px-20 px-5 bg-cover bg-center bg-no-repeat"
-          >
-               <div className="w-full pb-10 px-5">
-               <div className=" justify-center flex px-5 ">
-               <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSe4VS-tZICT267y9VBw0ca_rCQuV6aCJkgzKxBtLIiTrNAFiQ/viewform?embedded=true" 
-               className="w-full max-w-200 h-[2200px] rounded-md">
-               </iframe>
+          <div className="bg-fixed md:px-20 px-5 bg-cover bg-center bg-no-repeat">
+               <div className="w-full pb-10">
+                 <div className="flex justify-center">
+                   <iframe
+                     src="https://docs.google.com/forms/d/e/1FAIpQLSe4VS-tZICT267y9VBw0ca_rCQuV6aCJkgzKxBtLIiTrNAFiQ/viewform?embedded=true"
+                     className="w-full max-w-full h-[2610px] md:h-[2200px] rounded-md"
+                   ></iframe>
+                 </div>
                </div>
-
-
-               </div>
-         </div>   
+          </div>
     </div>
     );
 }
